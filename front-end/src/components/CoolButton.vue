6003ab0a1ef358d0a1bff90d8791cf7890af243a
M0xes2 <maximusa9@nycstudents.net>
2024-03-28 16:42:12 -0400

    baba
    albns

---
+++ front-end/src/components/CoolButton.vue
@@ -0,0 +1,9 @@
+<script setup>
+import { ref } from 'vue'
+
+const count = ref("a")
+</script>
+
+<template>
+  <button @click="count+=`a`">You clicked me {{ count }} times.</button>
+</template>
---
+++ front-end/src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <CoolButton />
+    <ul>
+      <li v-for="game in games" :key="game">{{ game }}</li>
+    </ul>
+    <h1>Hiii</h1>
+  </div>
+</template>
+
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+import CoolButton from "../components/CoolButton.vue"
+let games = ["Portal", "Inscryption", "Fish"]
+</script>
+
+
