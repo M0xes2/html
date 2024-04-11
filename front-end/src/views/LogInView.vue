bf5bb339fb48646e162f0498c0ff5cb4113c78c5
M0xes2 <158515668+M0xes2@users.noreply.github.com>
2024-04-10 22:36:06 -0400

    login + signup
    yes

---
+++ front-end/src/views/LogInView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>Log In</h1>
+    <input v-model="user" placeholder="Username" />
+    <input v-model="pass" placeholder="Password" />
+    <button @click="logIn(user, pass)">Log In</button>
+  </div>
+</template>
+
+<script setup>
+import { onMounted, ref } from "vue";
+const pages = ref("");
+async function logIn(username, password) {
+  console.log(username);
+  let res = await fetch("http://localhost:3000/login", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      username: username,
+      password: password,
+    }),
+  });
+
+  let data = await res.json();
+  pages.value = data;
+  console.log(data);
+}
+</script>
---
+++ front-end/src/views/SignUpView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>Sign up</h1>
+    <input v-model="user" placeholder="Username" />
+    <input v-model="pass" placeholder="Password" />
+    <button @click="signUp(user, pass)">Sign Up!</button>
+  </div>
+</template>
+
+<script setup>
+import { onMounted, ref } from "vue";
+const pages = ref("");
+async function signUp(username, password) {
+  console.log(username);
+  let res = await fetch("http://localhost:3000/signup", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      username: username,
+      password: password,
+    }),
+  });
+
+  let data = await res.json();
+  pages.value = data;
+  console.log(data);
+}
+</script>
