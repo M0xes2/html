bf2a4495c645a7438a66dc419b342a730380f425
Jacky <113130157+JagiZheng@users.noreply.github.com>
2024-05-16 15:58:11 -0400

    Update index.js

--- front-end/src/router/index.js
+++ front-end/src/router/index.js
@@ -82,12 +82,12 @@ const router = createRouter({
       component: () => import("../views/SignOutView.vue"),
     },
     {
-      path: '/LoginView',
-      name: 'LoginView',
+      path: '/LogView',
+      name: 'LogView',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/LoginView.vue')
+      component: () => import('../views/LogView.vue')
     },
     {
       path: '/settings',
