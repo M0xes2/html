75b2b2dc67b2776ed886d916de38b1e0e054f3a9
Jacky <113130157+JagiZheng@users.noreply.github.com>
2024-05-16 15:47:08 -0400

    Create user.js

---
+++ front-end/src/stores/user.js
@@ -0,0 +1,65 @@
+import { defineStore } from 'pinia'
+
+export const userStore = defineStore({
+  id: "user",
+  state: () => ({
+    user: {
+        username: "",
+        password: ""
+    },
+    count: 0,
+    edit: "",
+    text: "",
+    title:"",
+    error: "",
+    user_property:{
+        color: "",
+        font_size: ""
+    }
+  }),
+  actions: {
+    confirm(username, password){
+        this.user.username = username
+        this.user.password = password
+    },
+    check(username, password){
+        /*if(username = username from database and password = password from database){
+            this.user.username = username
+            this.user.password = password
+        }
+        else{
+            this.error = "Username or Password is incorrect"
+        }
+        */
+    },
+    increaseCount(){
+        this.count++
+    },
+    changeColor(color){
+        this.user_property.color = color;
+    },
+    changeFont(font_size){
+        this.user_property.font_size = font_size
+    },
+    addEditor(edit){
+        if (this.user.username == ""){
+        this.edit = "Cannot make changes without logging in first"
+        }
+        else{
+        this.edit = edit
+        this.text = this.user.username + " made a change on " + new Date();
+        }
+    },
+    changeTitle(title){
+        this.title = title;
+    },
+    logout(){
+        this.user.username = "";
+        this.user.password = "";
+        this.title = "";
+        this.edit = "";
+        this.user_property.color = "";
+        this.user_property.font = "";
+    }
+  }
+})
