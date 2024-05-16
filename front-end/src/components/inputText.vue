f5a3fb8bffa9942280cdababfa288dce220f3129
Jacky <113130157+JagiZheng@users.noreply.github.com>
2024-05-16 15:43:23 -0400

    Create inputText.vue

---
+++ front-end/src/components/inputText.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="card">
+    <div class="title">
+      <h2>{{ storeCounter.title }}</h2>
+    </div>
+    <div>
+      <h2 id="edit">{{ storeCounter.edit }}</h2>
+    </div>
+    <div class="username_display">
+      <h2>From: {{ storeCounter.user.username }}</h2>
+    </div>
+  </div>
+</template>
+
+<script setup>
+  import { userStore } from "@/stores/user";
+  const storeCounter = userStore(); 
+</script>
+
+<style lang="scss" scoped>
+  .username_display{
+  position: fixed;
+  top: 90%;
+  left: 30%;
+  }
+  .title{
+    display: inline-block;
+    position: absolute;
+    margin: 10px;
+    text-align: center;
+    top: 5%;
+  }
+</style>
