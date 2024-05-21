5261e2b66e54d703f653cb2f6fab8d5852e51532
M0xes2 <158515668+M0xes2@users.noreply.github.com>
2024-05-20 23:12:06 -0400

    Update UserView.vue

--- front-end/src/views/UserView.vue
+++ front-end/src/views/UserView.vue
@@ -1,9 +1,15 @@
 <template>
   <div>
     <h1>Hi {{ username }}!</h1>
-    <form action="http://localhost:3000/uploadPFP" method="POST" enctype="multipart/form-data">
-      <input type="file"  name="photo">
-      <input type="submit">
+    <iframe name="dummyframe" id="dummyframe" style="display: none"></iframe>
+    <form
+      target="dummyframe"
+      action="http://localhost:3000/uploadPFP"
+      method="POST"
+      enctype="multipart/form-data"
+    >
+      <input type="file" name="photo" accept="image/*" />
+      <input type="submit" />
     </form>
   </div>
 </template>
@@ -16,13 +22,37 @@ const router = useRouter();
 const userStore = useUserStore();
 const username = ref("");
 const pfp = ref("");
-
+const file = ref("");
 async function confirmUser() {
-  const data = await userStore.confirmUser()
+  const data = await userStore.confirmUser();
   if (!data) {
     router.push("/login");
   }
   username.value = data.user.username;
+}
+
+function importData() {
+  let input = document.createElement("input");
+  input.type = "file";
+  input.accept = "image/*";
+  input.onchange = (_) => {
+    // you can use this method to get file and perform respective operations
+    file.value = Array.from(input.files)[0];
+    console.log(file.value);
+    setProfile(Array.from(input.files)[0]);
+  };
+  input.click();
+}
+
+async function setProfile(img_file) {
+  let res = await fetch(`http://localhost:3000/uploadPFP`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: userStore.getUser().token,
+    },
+    body: { photo: img_file },
+  });
 }
 
 onMounted(() => {
